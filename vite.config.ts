998b0647fda87c40de8bf98dd7286625c910af88
mvop181 <xomyuk.danila@gmail.com>
2026-05-03 11:45:29 +0200

    fix: configure Vite for WASM files

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: { port: 5173, open: true },
   build: { target: 'es2022', sourcemap: true },
+  optimizeDeps: {
+    exclude: ['@dimforge/rapier3d-compat'],
+  },
+  worker: {
+    format: 'es',
+  },
 });
